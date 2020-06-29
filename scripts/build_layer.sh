30f742d17b3f96b4e42028b60da8a50b5fd4ae3f
gnseo <kieat.seo.kr@gmail.com>
2020-06-29 13:05:20 +0900

    add Pipfile, Pipfile.lock to layer

--- scripts/build_layer.sh
+++ scripts/build_layer.sh
@@ -1,5 +1,7 @@
 rm -f layer.zip
 rm -rf python
 pipenv lock -r | sed 's/-e //g' | pipenv run pip install --upgrade -r /dev/stdin --target python
+cp Pipfile ./python/Pipfile
+cp Pipfile.lock ./python/Pipfile.lock
 zip -r layer.zip python
 rm -rf python
