022b862ef4aba42ab376e8b0deb0234c85cc197a
gnseo <kieat.seo.kr@gmail.com>
2020-07-02 11:24:56 +0900

    update archive.sh

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf latest.zip
-zip -r latest.zip ./ -
-# aws lambda update-function-code --function-name odata-proxy --zip-file fileb://latest.zip --profile workuser
+zip -r latest.zip ./ -i "app.py"
+aws lambda update-function-code --function-name odata-proxy --zip-file fileb://latest.zip --profile mfa
 # To set aws config profile as default, we must export AWS_PROFILE environment variable with the profile name.
 # eg. export AWS_PROFILE=<profile name>
